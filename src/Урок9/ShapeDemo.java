465429e5c83af365307f4247f3c14281f31602ff
Scupus <loalantref@gmail.com>
2023-04-23 12:17:24 +1200

    Учимся 23.04.23

---
+++ src/Урок9/ShapeDemo.java
@@ -0,0 +1,12 @@
+package Урок9;
+
+public class ShapeDemo {
+    public static void main(String[] args) {
+        Shape[] shapes = new Shape[5];
+        shapes[0] = new Circle("green", 4, 3, 12);
+        shapes[1] = new Circle("blue", 5, 2, 10);
+        shapes[2] = new Rectangle("brown", 1, 2, 10,6);
+        shapes[3] = new Rectangle("white", 3, 2, 8,2);
+
+    }
+}
